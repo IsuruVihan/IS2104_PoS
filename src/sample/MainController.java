808f6b10cda543bb204b821cd854925a0f8edd7b
IsuruVihan <isuruvihan@gmail.com>
2021-07-25 12:43:44 +0530

    Initial commit

---
+++ src/sample/MainController.java
@@ -0,0 +1,23 @@
+package sample;
+
+import javafx.event.ActionEvent;
+
+public class MainController {
+
+  public void showCashier(ActionEvent event) {
+    System.out.println("Cashier");
+  }
+
+  public void showProducts(ActionEvent event) {
+    System.out.println("Products");
+  }
+
+  public void showSales(ActionEvent event) {
+    System.out.println("Sales");
+  }
+
+  public void showCoupons(ActionEvent event) {
+    System.out.println("Coupons");
+  }
+
+}
